d90a71a46c61444da55b38203d49ba1af4b13d64
gaoyulong <gaoyulong1996@163.com>
2023-05-01 14:38:28 +0800

    accomplish lab03

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -27,7 +27,15 @@ main:
 # The return value should be stored in a0
 factorial:
     # YOUR CODE HERE
-
+    addi t0 x0 1
+    addi t1 x0 1
+loop:
+    blt a0 t1 done
+    mul t0 t0 a0
+    addi a0 a0 -1
+    j loop
+done:
+    add a0 x0 t0
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra
