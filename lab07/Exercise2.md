4bb939c9b6c6c31ad48c4a1f9199ec2f73a29222
gaoyulong <gaoyulong@xiaomi.com>
2023-06-02 20:21:56 +0800

    accomplish lab 07

---
+++ lab07/Exercise2.md
@@ -0,0 +1,33 @@
+# 计算步长
+ijk:
+* A: n
+* B: 1
+* C: 0
+
+ikj:
+* A: 0
+* B: n
+* C: n
+
+jik:
+* A: n
+* B: 1
+* C: 0
+
+jki:
+* A: 1
+* B: 0
+* C: 1
+
+kij:
+* A: 0
+* B: n
+* C: n
+
+kji:
+* A: 1
+* B: 0
+* C: 1
+
+jki kji 最快
+ikj kij 最慢
--- lab07/transpose.c
+++ lab07/transpose.c
@@ -9,8 +9,23 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
     }
 }
 
+int inline min(int a, int b) {
+    return a > b ? b : a;
+}
+void inline Tblock(int n, int bkx, int bky, int i, int j, int *dst, int *src) {
+    int x, y;
+    for (x = i; x < i + bkx; x++)
+        for (y = j; y < j + bky; y++)
+            dst[y + x * n] = src[x + y * n];
+}
 /* Implement cache blocking below. You should NOT assume that n is a
  * multiple of the block size. */
 void transpose_blocking(int n, int blocksize, int *dst, int *src) {
     // YOUR CODE HERE
+    int x, y;
+    for (x = 0; x < n; x += min(n - x, blocksize)) {
+        for (y = 0; y < n; y += min(n - y, blocksize)) {
+            Tblock(n, min(n - x, blocksize), min(n - y, blocksize), x, y, dst, src);
+        }
+    }
 }
